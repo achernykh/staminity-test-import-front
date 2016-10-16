25e9ab35f497fe1a4ca96740e79ebe87af3ca9c9
Alexander Chernykh <chernykh@me.com>
2016-10-16 10:32:42 +0300

    prepair for merge with ui-router

--- src/js/calendar/index.js
+++ src/js/calendar/index.js
@@ -2,7 +2,7 @@ import Calendar from './calendar.component';
 import { CalendarDay } from './day/calendar.day.component.js'
 import { CalendarActivity } from './item/calendar.activity.component.js'
 
-let calendarModule = angular.module('staminity.calendar',['ui.scroll', 'ui.scroll.jqlite', 'dndLists']);
+let calendarModule = angular.module('staminity.calendar',[]);
     calendarModule.component('calendarDay', CalendarDay);
     calendarModule.component('calendarActivity', CalendarActivity);
     calendarModule.component('calendar', Calendar);
