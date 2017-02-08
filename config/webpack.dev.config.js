df182fa0c07db8ed1250b96ba5ef9184c0871890
anton <asdfghjkley@mail.ru>
2017-02-08 06:33:04 +0000

    fix

--- config/webpack.dev.config.js
+++ config/webpack.dev.config.js
@@ -5,7 +5,7 @@ var ENV = process.env.npm_lifecycle_event;
 
 module.exports = {
     output: {
-        publicPath: 'http://localhost:8080/',
+        publicPath: 'http://staminity-frontend-asdfghjkley.c9users.io/',
         filename: 'assets/js/[name].js',
         chunkFilename: 'assets/js/[name].js'
     },
@@ -31,6 +31,8 @@ module.exports = {
         new webpack.optimize.CommonsChunkPlugin({ name: 'vendor', filename: 'assets/js/[name].js' })
     ],
     devServer: {
+        host: '0.0.0.0',
+        port: 8080,
         contentBase: './src',
         stats: 'minimal',
         /*stats: {
