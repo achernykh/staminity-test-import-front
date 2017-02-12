e7f8559659ad2296f4894b6f04c3d23838479668
CapB1ack <kor_sar@mail.ru>
2017-02-12 21:13:42 +0300

    dashboards fix

--- src/app/dashboard/total/dashboard-total.component.js
+++ src/app/dashboard/total/dashboard-total.component.js
@@ -2,25 +2,85 @@ import moment from 'moment/src/moment';
 import * as angular from 'angular';
 
 class DashboardTotalCtrl {
-    constructor($mdDialog,ActionMessageService, ActivityService, $scope){
-        this.$mdDialog = $mdDialog;
-        this.ActionMessageService = ActionMessageService;
-        this.ActivityService = ActivityService;
-        this.$scope = $scope;
+    constructor(){
+
+        this.checked = false;
+        this.total = {};
+        this.totalTemplate = {
+            completed: {
+                distance: 0,
+                movingDuration: 0
+            },
+            planned: {
+                distance: 0,
+                movingDuration: 0
+            }
+        }
+        this.primarySport = ['run', 'bike', 'swim'];
     }
-    $onInit() {
+    $onInit(){
+        this.weekTitle = moment(this.week.week,'YYYY-WW').week();
+    }
+    $onChanges(changes){
+
+        if(changes.update){
+            console.log('CalendarTotal: $onChanges => status new value', this.week.sid, this.week);
+            this.total = {};
+            let sport = null;
+            for (let day of this.week.subItem){
+                for (let item of day.data.calendarItems){
+                    if (item.calendarItemType == 'activity') {
+                        sport = item.activityHeader.activityType.typeBasic
+                        //template = {completed: {distance: 0,movingDuration: 0},planned: {distance: 0,movingDuration: 0}}
+                        if (item.activityHeader.hasOwnProperty('intervals'))
+                            for (let interval of item.activityHeader.intervals) {
+                                if (interval.type == 'W') {
+                                    if(this.primarySport.indexOf(sport) == -1) sport = 'other'
+
+                                    if(!this.total.hasOwnProperty(sport))
+                                        Object.assign(this.total, {[sport]: {completed: {distance: 0,movingDuration: 0},planned: {distance: 0,movingDuration: 0}}})
+
+                                    this.total[sport].completed.distance = this.total[sport].completed.distance +
+                                        (interval.calcMeasures.distance.hasOwnProperty('maxValue') && interval.calcMeasures.distance.maxValue) || 0
+
+                                    this.total[sport].completed.movingDuration = this.total[sport].completed.movingDuration +
+                                        (interval.calcMeasures.movingDuration.hasOwnProperty('maxValue') && interval.calcMeasures.movingDuration.maxValue) || 0
+
+                                    //console.log('week total temp=',sport, this.total[sport].completed.distance)
+                                }
+                            }
+                    }
+
+                }
+            }
+            console.log('week total 1', this.total);
+            // Итоги по всем активностям недели
+            this.summary = {completed: {distance: 0,movingDuration: 0},planned: {distance: 0,movingDuration: 0}};
+            Object.keys(this.total).forEach((sport) => {
+                this.summary.completed.distance += this.total[sport].completed.distance;
+                this.summary.completed.movingDuration += this.total[sport].completed.movingDuration;
+                this.summary.planned.distance += this.total[sport].planned.distance;
+                this.summary.planned.movingDuration += this.total[sport].planned.movingDuration;
+
+            });
+            console.log('week total 2', this.total);
+        }
     }
 
+    secondToDuration(second) {
+        return moment().startOf('day').seconds(second).format('H:mm:ss')
+    }
 
 }
 DashboardTotalCtrl.$inject = ['$mdDialog','ActionMessageService', 'ActivityService', '$scope'];
 
 export let DashboardTotal = {
     bindings: {
-        data: '<',
+        week: '<',
+        update: '<',
         selected: '<',
         accent: '<',
-        onSelect: '&'
+        onToggle: '&'
     },
     require: {
         dashboard: '^dashboard'
