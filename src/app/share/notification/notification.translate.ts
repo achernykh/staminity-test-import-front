0ced24d6a99ab02962587d9ab929f6a059626d00
Evgeniy <e.zakhar@gmail.com>
2017-03-16 20:22:37 +0000

    Update notification.translate.ts

--- src/app/share/notification/notification.translate.ts
+++ src/app/share/notification/notification.translate.ts
@@ -20,7 +20,30 @@ export const translateNotification = {
          * data[2] - фактическая скорость (speed)
          * */
         notificationTestMessage: "Новое тестовое сообщение с параметром: расстояние {{data[1] | measureCalc:data[0]:'distance'}} " +
-        "{{'distance' | measureUnit:data[0] | translate}} со скоростью {{data[2] | measureCalc:data[0]:'speed'}} {{'speed' | measureUnit:data[0] | translate}}"
+        "{{'distance' | measureUnit:data[0] | translate}} со скоростью {{data[2] | measureCalc:data[0]:'speed'}} {{'speed' | measureUnit:data[0] | translate}}",
+        
+        /**
+        * Сообщение спортсмену, что его тренировка загружена. Отправитель - провайдер
+        * data[0] - базовый вид спорта
+        * data[1] - дата тренировки в формате DD.MM.YY
+        * data[2] - activityId (вопрос - или calendarItemId?). Будет использоваться для перехода к тренировке, когда сделаем адресацию
+        * */
+        uploadActivityByProvider: "Ваша тренировка за {{data[1]}} по виду спорта '{{data[0]} | translate}' загружена",
+
+        /**
+        * Сообщение тренеру, что тренировка его ученика загружена. Отправитель - спортсмен.
+        * data[0] - базовый вид спорта
+        * data[1] - дата тренировки в формате DD.MM.YY
+        * data[2] - activityId (вопрос - или calendarItemId?)
+        * data[3] - фактическая длительность тренировки = calcMeasures.duration.value
+        * data[4] - фактическое расстояние тренировки = calcMeasures.distance.value
+        * data[5] - фактический % выполнения. На всякий случай написал, но надо подумать, стоит ли выводить. Ведь потом спортсмен может менять границы сегментов
+        * и % выполнения, рассчитанный автоматически, может измениться.
+        * */
+        activityCompletedByAthlete: "Выполнена тренировка {{data[1]}} по виду спорта '{{data[0]} | translate}'. Общее время: {{data[3]}}, + 
+        " расстояние {{data[4] | measureCalc:data[0]:'distance'}} {{'distance' | measureUnit:data[0] | translate}}"
+
+        
     },
     en: {
 
