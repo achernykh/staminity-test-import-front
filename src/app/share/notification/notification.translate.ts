49a09fb061a5dc6cb7fe2ec34c40b0094474ecea
Evgeniy <e.zakhar@gmail.com>
2017-03-16 20:48:10 +0000

    Update notification.translate.ts

--- src/app/share/notification/notification.translate.ts
+++ src/app/share/notification/notification.translate.ts
@@ -26,24 +26,28 @@ export const translateNotification = {
         * Сообщение спортсмену, что его тренировка загружена. Отправитель - провайдер
         * data[0] - базовый вид спорта
         * data[1] - дата тренировки в формате DD.MM.YY
-        * data[2] - activityId (вопрос - или calendarItemId?). Будет использоваться для перехода к тренировке, когда сделаем адресацию
+        * data[2] - calendarItemId. Будет использоваться для перехода к тренировке, когда сделаем адресацию
+        * data[3] - фактическая длительность тренировки = calcMeasures.duration.value
+        * data[4] - фактическое расстояние тренировки = calcMeasures.distance.value
+        * data[5] - фактический % выполнения = calcMeasures.completePercent.value
+        * 
         * */
-        uploadActivityByProvider: "Ваша тренировка за {{data[1]}} по виду спорта '{{data[0]} | translate}' загружена",
+        uploadActivityByProvider: "Загружена тренировка {{data[1]}} \n " +
+        "{{data[0]} | translate}, {{data[4] | measureCalc:data[0]:'distance'}} {{'distance' | measureUnit:data[0] | translate}}, {{data[3]}}", 
+        
 
         /**
         * Сообщение тренеру, что тренировка его ученика загружена. Отправитель - спортсмен.
         * data[0] - базовый вид спорта
         * data[1] - дата тренировки в формате DD.MM.YY
-        * data[2] - activityId (вопрос - или calendarItemId?)
+        * data[2] - calendarItemId
         * data[3] - фактическая длительность тренировки = calcMeasures.duration.value
         * data[4] - фактическое расстояние тренировки = calcMeasures.distance.value
-        * data[5] - фактический % выполнения. На всякий случай написал, но надо подумать, стоит ли выводить. Ведь потом спортсмен может менять границы сегментов
-        * и % выполнения, рассчитанный автоматически, может измениться.
+        * data[5] - фактический % выполнения = calcMeasures.completePercent.value
         * */
-        activityCompletedByAthlete: "Выполнена тренировка {{data[1]}} по виду спорта '{{data[0]} | translate}'. Общее время: {{data[3]}}," + 
-        " расстояние {{data[4] | measureCalc:data[0]:'distance'}} {{'distance' | measureUnit:data[0] | translate}}"
+        activityCompletedByAthlete: "Выполнена тренировка {{data[1]}} \n "+
+        "{{data[0]} | translate}, {{data[4] | measureCalc:data[0]:'distance'}} {{'distance' | measureUnit:data[0] | translate}}, {{data[3]}}" 
 
-        
     },
     en: {
 
