382345059ec1ec596e3d567040edabbd9bd9b452
Evgeniy <e.zakhar@gmail.com>
2017-03-17 10:27:37 +0000

    Update notification.translate.ts

--- src/app/share/notification/notification.translate.ts
+++ src/app/share/notification/notification.translate.ts
@@ -13,7 +13,7 @@
  * */
 export const translateNotification = {
     ru: {
-        /** ----------ПРИМЕР --------
+/** ----------ПРИМЕР --------
          * Тестовое сообщение
          * data[0] - базовый вид спорта
          * data[1] - фактическое расстояние (distance)
@@ -22,10 +22,15 @@ export const translateNotification = {
         notificationTestMessage: "Новое тестовое сообщение с параметром: расстояние {{data[1] | measureCalc:data[0]:'distance'}} " +
         "{{'distance' | measureUnit:data[0] | translate}} со скоростью {{data[2] | measureCalc:data[0]:'speed'}} {{'speed' | measureUnit:data[0] | translate}}",
   
-      /** ----------Синхронизация фактических тренировок --------**/
-        initialProviderSyncCompleted: "",
-        initialProviderSyncErrors: "",
-      
+/** ----------Синхронизация фактических тренировок --------**/
+        /** Начальная загрузка завершена. Отправитель - провайдер
+        data[0] - количество загруженных тренировок
+        data[1] - общее количество тренировок  
+        data[2] - дата начальной синхронизации в формате DD.MM.YY**/
+        initialProviderSyncCompleted: "Начальная синхронизация завершена.\n" +
+        "Загружено {{data[0]}} из {{data[1]}} тренировок c {{data[2]}}",
+        
+
         /**Сообщение спортсмену, что его тренировка загружена. Отправитель - провайдер
         * data[0] - базовый вид спорта
         * data[1] - дата тренировки в формате DD.MM.YY
@@ -36,33 +41,60 @@ export const translateNotification = {
         uploadActivityByProvider: "Загружена тренировка {{data[1]}} \n " +
         "{{data[0]} | translate}, {{data[4] | measureCalc:data[0]:'distance'}} {{'distance' | measureUnit:data[0] | translate}}, {{data[3]}}", 
         
-      /** ---------- Тренировки --------**/
+/** ---------- Тренировки --------**/
 
-        /**Сообщение тренеру, что тренировка его ученика загружена. Отправитель - спортсмен.
+        /**Тренировка ученика загружена. Получатель - тренер, отправитель - спортсмен.
         * data[0] - базовый вид спорта
         * data[1] - дата тренировки в формате DD.MM.YY
         * data[2] - calendarItemId
         * data[3] - фактическая длительность тренировки = calcMeasures.duration.value
         * data[4] - фактическое расстояние тренировки = calcMeasures.distance.value
-        * data[5] - фактический % выполнения = calcMeasures.completePercent.value
-        * */
+        * data[5] - фактический % выполнения = calcMeasures.completePercent.value**/
         activityCompletedByAthlete: "Выполнена тренировка {{data[1]}} \n "+
-        "{{data[0]} | translate}, {{data[4] | measureCalc:data[0]:'distance'}} {{'distance' | measureUnit:data[0] | translate}}, {{data[3]}}" 
+        "{{data[0]} | translate}, {{data[4] | measureCalc:data[0]:'distance'}} {{'distance' | measureUnit:data[0] | translate}}, {{data[3]}}", 
 
-        activityCompletedByFriend: "",
-        activityCompletedByFollowing: "",
+        /** Плановая тренировка создана тренером. Получатель - спортсмен, отправитель - тренер
+        * data[0] - базовый вид спорта
+        * data[1] - вид спорта
+        * data[2] - плановая дата тренировки в формате DD.MM.YY
+        * data[3] - calendarItemId
+        * data[4] - название типа тренировки**/
+        activityCreatedByCoach: "Создана плановая тренировка: "+
+        "{{data[1]} | translate}, {{data[2]}}, {{data[4]} | translate}",
+        
+         /** Плановая тренировка изменена тренером. Получатель - спортсмен, отправитель - тренер
+        * data[0] - базовый вид спорта
+        * data[1] - вид спорта
+        * data[2] - плановая дата тренировки в формате DD.MM.YY
+        * data[3] - calendarItemId
+        * data[4] - название типа тренировки**/       
+        activityModifiedByCoach: "Изменена плановая тренировка: "+
+        "{{data[1]} | translate}, {{data[2]}}, {{data[4]} | translate}",
 
-        activityCreatedByCoach: "",
-        activityModifiedByCoach: "",
+         /** Факт по тренировке изменен спортсменом. Условия получения такого уведомления: 
+            - спортсмен ввел или изменил факт вручную в тренировке,
+            - спортсмен откорректировал фактическую длительность сегментов,
+            Получатель - тренер, отправитель - спортсмен.
+        * data[0] - базовый вид спорта
+        * data[1] - вид спорта
+        * data[2] - фактическая дата тренировки в формате DD.MM.YY
+        * data[3] - calendarItemId
+        * data[4] - название типа тренировки**/ 
+        activityFactModifiedByAthlete: "Изменен факт в тренировке: " +
+        "{{data[1]} | translate}, {{data[2]}}, {{data[4]} | translate}",
+
+        activityCompletedByFriend: "", /** под вопросом. Тренировки друзей достаточно видеть в ленте **/
+        activityCompletedByFollowing: "",  /** аналогично, под вопросом **/
+
         
-        activityModifiedByAthlete: "",
-        
-    /** ---------- Комментарии и лайки  --------**/
+    /** ---------- Комментарии, лайки, сообщения  --------**/
     newCoachCommentSingle: "",
     newCoachComments: "",
     newUserCommentSingle: "",
     newLikeSingle: "",
     newUserCommentsAndLikes: "",
+    newSingleMessage: "",
+    newMessages: "",
 
     /** ---------- Принятие и отклонение запросов пользователей  --------**/
     requestStartCoachingApproved: "",
@@ -79,8 +111,19 @@ export const translateNotification = {
     
     requestJoinUserGroupApproved: "",
     requestJoinUserGroupDeclined: "",
+    requestAddFriendApproved: "",
+
+/** ---------- Оповещения в клубе  --------**/
+    clubAthleteConnected: "",
+    clubAthleteDisconnected: "",
+    clubCoachAssigned: "",
+    clubCoachRemoved: "",
+
+    clubRoleAssigned: "",
+    clubRoleRemoved: "",
     
-    /** ---------- Отключение от тренеров, выход из клубов, выход из групп  --------**/
+    
+/** ---------- Отключение от тренеров, выход из клубов, выход из групп  --------**/
     leaveCoachByAthlete: "",
     leaveCoachByCoach: "",
     leaveClubByAthlete: "",
@@ -111,7 +154,7 @@ export const translateNotification = {
     /** ----------Зоны и пороги --------**/
         zonesChangedByCoach: "",
         zonesChangedByAthelete: "",
-        zonesChangeRequestByService: "",
+        zonesChangeRequestByService: ""
         
     /** ----------Зоны и пороги --------**/
 
