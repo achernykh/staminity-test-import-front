e00900980aee59a44268b2fce182c2f2cfb98606
Alexander Chernykh <chernykh@me.com>
2017-06-10 19:44:51 +0300

    new service-worker

---
+++ src/app/core/env.js
@@ -0,0 +1,19 @@
+var build = '58';
+var version = '0.5.1-beta';
+var protocol = {
+  ws: 'wss://',
+  rest: 'https://'
+};
+var server = 'app.staminity.com';
+var content = 'https://app.staminity.com';
+var frontend = 'http://0.0.0.0:8080/';
+
+
+module.exports = {
+  build: build,
+  version: version,
+  protocol: protocol,
+  server: server,
+  content: content,
+  frontend: frontend
+}
