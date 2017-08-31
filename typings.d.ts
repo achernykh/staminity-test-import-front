b9f822e11556d21f80751cc7185b52b569f64b41
Emil Boev <kycok.6o6pa@gmail.com>
2017-08-31 23:34:21 +0300

    support Typescript 2.2 and newer

--- typings.d.ts
+++ typings.d.ts
@@ -1,8 +1,10 @@
 declare var __ENV: string;
 declare var angular;
-declare var require: {
-    
-    <T>(path: string): T;
-    (paths: string[], callback: (...modules: any[]) => void): void;
-    ensure: (paths: string[], callback: (require: <T>(path: string) => T) => void, name?: string) => void;
-};
+
+interface WebpackRequire {
+  <T>(path: string): T;
+  (paths: string[], callback: (...modules: any[]) => void): void;
+  ensure: (paths: string[], callback: (require: <T>(path: string) => T) => void) => void;
+}
+interface NodeRequire extends WebpackRequire {}
+declare var require: NodeRequire;
