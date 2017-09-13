fe0b35a987ea1652ef79524a861e545435ce358b
Alexander Chernykh <chernykh@me.com>
2017-09-13 16:55:25 +0300

    fix for api/mobileapp (copy api/develop release 1m ago)

--- src/app/core/user.function.ts
+++ src/app/core/user.function.ts
@@ -1,8 +1,8 @@
-import {IUserProfileShort, IUserProfile, ITrainingZonesType} from "../../../api/user/user.interface";
+import {IUserProfileShort, IUserProfile, ITrainingZonesType, ITrainingZones} from "../../../api/user/user.interface";
 
 export const profileShort = (user: IUserProfile):IUserProfileShort => ({userId: user.userId, public: user.public});
 
-export const getFTP = (zones: Array<ITrainingZonesType>, measure: string, sport: string):number => {
+export const getFTP = (zones: ITrainingZones, measure: string, sport: string):number => {
     return  (zones.hasOwnProperty(measure) && zones[measure].hasOwnProperty(sport) && zones[measure][sport]['FTP']) ||
             (zones.hasOwnProperty(measure) && zones[measure].hasOwnProperty('default') && zones[measure]['default']['FTP']) || null;
 };
