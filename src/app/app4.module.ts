1e1b5b25a8366e2995bd466fa2cdca1fb104b1a1
Alexander Chernykh <chernykh@me.com>
2017-10-08 15:59:46 +0300

    install npm

---
+++ src/app/app4.module.ts
@@ -0,0 +1,20 @@
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import { UpgradeModule } from '@angular/upgrade/static';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+@NgModule({
+    imports: [
+        BrowserModule,
+        UpgradeModule
+    ]
+})
+
+export class AppModule {
+    constructor(private upgrade: UpgradeModule) { }
+    ngDoBootstrap() {
+        this.upgrade.bootstrap(document.body, ['heroApp'], { strictDi: true });
+    }
+}
+
+platformBrowserDynamic().bootstrapModule(AppModule);
