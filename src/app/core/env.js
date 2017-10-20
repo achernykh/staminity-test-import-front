f57215d700270e5a53cad653102c2ec00f006fff
Alexander Chernykh <chernykh@me.com>
2017-10-20 14:01:04 +0300

    upd

--- src/app/core/env.js
+++ src/app/core/env.js
@@ -1,4 +1,4 @@
-var build = '250';
+var build = '256';
 var version = '1.0.2';
 var protocol = {
   ws: 'wss://',
--- src/app4/share/application-menu/application-menu.component.ts
+++ src/app4/share/application-menu/application-menu.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from "@angular/core";
 import { IUserProfile } from "../../../../api/user/user.interface";
 import { UserBackgroundPipe } from '../pipes/user-background.pipe';
+import { DomSanitizer, SafeStyle } from "@angular/platform-browser";
 
 @Component({
     selector: 'st-application-menu',
@@ -12,17 +13,15 @@ export class ApplicationMenuComponent {
 
     @Input() user: IUserProfile;
 
-    constructor(private UserBackgroundPipe: UserBackgroundPipe) {
+    constructor(private UserBackgroundPipe: UserBackgroundPipe,
+                private sanitizer: DomSanitizer) {
 
     }
 
-    get userBackground(): Object {
-        return {
-            'background-image':
-                `linear-gradient(to bottom, rgba(0,0,0, 0.1), rgba(0, 0, 0, 0.2) 30%, 
-                rgba(0, 0, 0, 0.4) 60%, rgba(0, 0, 0, 0.5)),
-                url(${this.UserBackgroundPipe.transform(this.user.public.background)})`,
-            'background-size': 'cover'
-        };
+    userBackground(url: string): SafeStyle {
+        return this.sanitizer.bypassSecurityTrustStyle(
+            `linear-gradient(to bottom, rgba(0,0,0, 0.1), rgba(0, 0, 0, 0.2) 30%, rgba(0, 0, 0, 0.4) 60%, rgba(0, 0, 0, 0.5)), 
+            url(${this.UserBackgroundPipe.transform(url)})`);
     }
+
 }
