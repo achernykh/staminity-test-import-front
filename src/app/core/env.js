c6dc1f229650213758357e72e1bc86ba63896081
Unknown <a.platov@mail.ru>
2017-11-25 14:54:08 +0600

    /mobile: Auto stash before cherrypick of "bottom sheet fix"

--- src/app/core/env.js
+++ src/app/core/env.js
@@ -4,9 +4,9 @@ var protocol = {
   ws: 'wss://',
   rest: 'https://'
 };
-var server = 'app.staminity.com';
+var server = 'testapp.staminity.com:8080';
 var content = 'https://app.staminity.com';
-var frontend = 'https://staminity.com/';
+var frontend = 'https://dev2.staminity.com/';
 
 
 module.exports = {
