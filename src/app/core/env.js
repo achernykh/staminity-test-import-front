185382dad502da0b7358110ef738b16e03b1d4bd
Alexander Chernykh <chernykh@me.com>
2017-11-28 17:53:44 +0300

    new version

--- src/app/core/env.js
+++ src/app/core/env.js
@@ -1,12 +1,12 @@
-var build = '238';
-var version = '1.4.2';
+var build = '240';
+var version = '1.4.3';
 var protocol = {
   ws: 'wss://',
   rest: 'https://'
 };
-var server = 'testapp.staminity.com:8080';
-var content = 'https://testapp.staminity.com:8080';
-var frontend = 'http://dev1.staminity.com/';
+var server = 'app.staminity.com';
+var content = 'https://app.staminity.com';
+var frontend = 'https://staminity.com/';
 
 
 module.exports = {
