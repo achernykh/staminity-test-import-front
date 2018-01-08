321ce8472c6135f4f0b5a1aeda6f8c25fa5bdc08
Unknown <a.platov@mail.ru>
2018-01-08 23:31:50 +0600

    wip

--- src/app/user/settings/user-settings.service.ts
+++ src/app/user/settings/user-settings.service.ts
@@ -1,14 +1,18 @@
 import { element } from 'angular';
+import DisplayService from "../../core/display.service";
+import { countriesList } from './user-settings.constants';
 import { UserSettingsPasswordCtrl } from './user-settings-password/user-settings-password.dialog';
 
 export class UserSettingsService {
 
-    static $inject = ['$mdDialog', 'message', 'AuthService'];
+    static $inject = ['$http', '$mdDialog', 'message', 'AuthService', 'DisplayService'];
 
     constructor (
+        private $http,
         private $mdDialog,
         private message,
         private authService,
+        private displayService: DisplayService,
     ) {
 
     }
@@ -37,5 +41,39 @@ export class UserSettingsService {
                 this.message.systemWarning(info);
             });
         });
+    }
+
+    /**
+     * Поиск страны
+     * @param query: string
+     * @returns {string[]}
+     */
+    countrySearch (query: string) : string[] {
+        let countries = countriesList[this.displayService.getLocale()];
+        let regexp = new RegExp(query, 'i');
+
+        return query ? Object.keys(countries).filter((key) => ~countries[key].search(regexp)) : countries;
+    }
+
+    /**
+     * Поиск города
+     * @param query: string
+     * @returns {Promise<any>}
+     */
+    citySearch (query: string) : Promise<any> {
+        let language = this.displayService.getLocale();
+        let api = 'https://maps.googleapis.com/maps/api/place/autocomplete/json';
+        let key = 'AIzaSyAOt7X5dgVmvxcx3WCVZ0Swm3CyfzDDTcM'
+        let request = {
+            method: 'GET',
+            url: `${api}?input=${query}&types=(cities)&language=${language}&key=${key}`,
+            headers: {
+                'Access-Control-Allow-Headers': 'Content-Type, Content-Range, Content-Disposition, Content-Description',
+                'Access-Control-Allow-Methods': 'GET, PUT, POST, DELETE, OPTIONS',
+                'Access-Control-Allow-Origin': '*'
+            }
+        };
+
+        return this.$http(request).then((result) => result.predictions, (error) => []);
     }
 }
