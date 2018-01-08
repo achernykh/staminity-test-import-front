eae193b66531446c40f6724ba076987166107d11
Unknown <a.platov@mail.ru>
2018-01-08 08:55:11 +0600

    wip

--- src/app/user/settings/user-settings-coach/user-settings-coach.datamodel.ts
+++ src/app/user/settings/user-settings-coach/user-settings-coach.datamodel.ts
@@ -4,26 +4,24 @@ export class UserSettingsCoachDatamodel {
 
 	firstName: string;
 	lastName: string;
-	uri: string;
+	about: string;
 	country: string;
 	city: string;
-	dateOfBirth: Date;
-	sex: string;
-	about: string;
-	email: string;
-	phone: string;
+	price: string;
+	contact: string;
+	athletes: string;
+	isFree: boolean;
 
 	constructor (private profile: IUserProfile) {
 		this.firstName = profile.public.firstName;
 		this.lastName = profile.public.lastName;
-		this.uri = profile.public.uri;
 		this.about = profile.personal['about'];
 		this.country = profile.personal['country'];
 		this.city = profile.personal['city'];
-		this.dateOfBirth = profile.personal.birthday;
-		this.sex = profile.personal.sex;
-		this.email = profile.personal.extEmail;
-		this.phone = profile.personal.phone;
+		this.price = profile.personal['price'];
+		this.contact = profile.personal['contact'];
+		this.athletes = profile.personal['athletes'];
+		this.isFree = profile.personal['isFree'];
 	}
 
 	toUserProfile () : IUserProfile {
@@ -34,17 +32,16 @@ export class UserSettingsCoachDatamodel {
 				...this.profile.public,
 				firstName: this.firstName,
 				lastName: this.lastName,
-				uri: this.uri,
 		    },
 		    personal: {
 		    	...this.profile.personal,
 		    	about: this.about,
 		    	country: this.country,
 		    	city: this.city,
-		        sex: this.sex,
-		        birthday: this.dateOfBirth,
-		        extEmail: this.email,
-		        phone: this.phone,
+		        price: this.price,
+		        contact: this.contact,
+		        athletes: this.athletes,
+		        isFree: this.isFree,
 		    },
 		} as any;
 	}
