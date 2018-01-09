1f748aed94ca653debe23c790f38ebd5ebcaa845
Unknown <a.platov@mail.ru>
2018-01-10 01:48:43 +0600

    /hotfix: cache

--- src/app/core/env.js
+++ src/app/core/env.js
@@ -1,12 +1,12 @@
-var build = '363';
+var build = '367';
 var version = '2.0.1';
 var protocol = {
   ws: 'wss://',
   rest: 'https://'
 };
-var server = 'app.staminity.com';
-var content = 'https://app.staminity.com';
-var frontend = 'https://staminity.com/';
+var server = 'testapp.staminity.com:8080';
+var content = 'https://testapp.staminity.com:8080';
+var frontend = 'http://dev2.staminity.com/';
 
 
 module.exports = {
