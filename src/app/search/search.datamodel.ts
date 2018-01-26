f735fd1c3036e844ad3d75b52fd3a88951278c5f
Denis Ivanov <ivanovdns@gmail.com>
2018-01-26 22:14:25 +0300

    SearchFactory mockup

--- src/app/search/search.datamodel.ts
+++ src/app/search/search.datamodel.ts
@@ -1,64 +1,78 @@
 import { SearchResultByUser } from "@api/search/search.interface";
+import { SocketService } from "../core";
 
 export class SearchModule {
-    usp: SearchPanel<SearchResultByUser, UserSearchFilter>;
+    usp: SearchFactory<SearchResultByUser, UserSearchFilter>;
 
     constructor () {
-        let CoachSearchPanel: SearchPanel<CollectionItemDataInterfaceCoach, CoachSearchFilter>;
-        //let ClubSearchPanel: SearchPanel<CollectionItemDataInterfaceCoach, ClubSearchFilter>;
 
-        //UserSearchPanel.init({param1: "value1"});
-
-        //console.log(UserSearchPanel.getList());
-        console.log(CoachSearchPanel.getList());
     }
 
-    lala(): void {
+    lala (): void {
         this.usp.init({});
         this.usp.filterData.nameSubstring = "lala";
     }
 }
 
-class SearchPanel<T, F extends CollectionFilter> implements Collection<T, F> {
+class SearchFactory<T, F extends CollectionFilter> implements Collection<T, F>, Iterable<T> {
     itemList: Array<T>;
     filterData: F;
+    curPosition: number;
+    socket: SocketService;
 
     init (filterData): this {
+        this.curPosition = -1; //
         this.filterData = filterData;
         return this;
     }
 
+    isEmpty (): boolean {
+        return this.itemList.length > 0;
+    }
+
     getCount (): number {
-        return null;
+        return this.itemList.length;
     }
 
     push<T> (itemList: Array<T>): void {
+        // FIXME нужна реализация метода, прнимающего на вход как отдельный элемент, так и список
     }
 
     clear (): void {
-        this.itemList.forEach(e => this.delete(e));
+        this.itemList.forEach(e => this.remove(e));
     }
 
     getItem (idx: number): T {
-        let retVal: T;
-        retVal = this.itemList[ idx ];
-        return retVal;
+        return this.itemList[ idx ];
     }
 
     getList (): Array<T> {
-        return null;
+        return this.itemList;
     }
 
-    delete<T> (item: T): T {
+    remove<T> (item: T): T {
         return item;
     }
 
-    getBackendData(): void {
-
+    searchBackendData (): void {
+        if (this.filterData.requestType.length === 0) {
+            // FIXME нужно обработать вариант, когда программист забыл явно указать тип запроса, выполняемого в бэке
+        }
+        // FIXME возможно тут ошибка в распознавании массива и единичного элемента
+        this.socket.send(this.filterData).then((result) => {
+            if (result.hasOwnProperty("arrayResult")) {
+                this.itemList.push(result.arrayResult);
+            } else {
+                this.itemList.push(result);
+            }
+        });
     }
 
-    getNextBackendData (): void {
-
+    searchNextBackendData (): void {
+        // ищем элементы дальше тех, которые уже к нам загружена согласно условиям
+        this.filterData.offset = this.getCount();
+        // вызываем поиск еще раз
+        this.searchBackendData();
     }
 
     getFilterParams (): any {
@@ -66,39 +80,64 @@ class SearchPanel<T, F extends CollectionFilter> implements Collection<T, F> {
     }
 
     setFilterParameter (parameterName: string, parameterValue: any): F {
-        return this.filterData.params;
+        // FIXME необходимо реализовать динамическое присвоение значений полям, имена которых передаются в параметре метода
+        return this.filterData;
     };
+
+    hasNext (): boolean {
+        return !this.isEmpty() && this.curPosition < this.getCount();
+    }
+
+    getNext (): T {
+        return this.getItem(this.curPosition++);
+    }
 }
-
-export interface CollectionItemDataInterfaceCoach {
-
-}
-
-export interface CollectionItemDataInterfaceClub {
-
-}
-
 
 export interface UserSearchFilter extends CollectionFilter {
+    requestType: "searchUsersAndGroups";
     nameSubstring: string;
 
-    setNameSubstring(str: string) : ClubSearchFilter;
+    setNameSubstring(str: string): UserSearchFilter;
 }
 
 export interface CoachSearchFilter extends CollectionFilter {
     nameSubstring: string;
+    readyForHire: boolean;
 }
 
-export interface ClubSearchFilter {
+export interface ClubSearchFilter extends CollectionFilter {
     nameSubstring: string;
+}
+
+/**
+ * Базовый интерфейс итератора
+ */
+interface Iterable<T> {
+    /**
+     * Текущая позиция итератора относительно коллекции
+     */
+    curPosition: number;
+
+    /**
+     * Коллекция имеет след-й элемент относительно текущего
+     * @returns {boolean}
+     */
+    hasNext(): boolean;
+
+    /**
+     * Получение следующего элемента из коллекции
+     * @returns {T}
+     */
+    getNext(): T;
 }
 
 /**
  * Базовый набор параметров фильтрации списков на основе страничной загрузки данных
  */
 export interface CollectionFilter {
+    requestType: string;
     limit: number;
-    params: any;
+    offset: number;
 
     setFilterParameter(parameterName: string, parameterValue: any): CollectionFilter;
 
@@ -119,14 +158,20 @@ export interface Collection<T, F extends CollectionFilter> {
     init<T, CF>(filter: CF): this;
 
     /**
+     * Коллекция не содержит элементов
+     * @returns {boolean}
+     */
+    isEmpty(): boolean;
+
+    /**
      * Кол-во элементов в списке
      * @returns {number}
      */
     getCount(): number;
 
     /**
-     * Инициализация коллекции по списку элементов
-     * @param {Array<any>} itemList
+     * Добавление в коллекцию списка элементов
+     * @param {Array<T>} itemList
      */
     push<T>(itemList: Array<T>): void;
 
@@ -153,17 +198,17 @@ export interface Collection<T, F extends CollectionFilter> {
      * @param {T} item
      * @returns {T}
      */
-    delete<T>(item: T): T;
+    remove<T>(item: T): T;
 
     /**
      * Загрузка данных в коллекцию из бэка на основе параметров
      */
-    getBackendData(): void;
+    searchBackendData(): void;
 
     /**
      * Загрузка следующей пачки данных в коллекцию на основе того же набора параметров фильтрации
      */
-    getNextBackendData(): void;
+    searchNextBackendData(): void;
 
     setFilterParameter(parameterName: string, parameterValue: any): F;
 
