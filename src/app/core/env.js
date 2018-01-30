b52d9d67043efd1702dac0da66a46e1de373bdc9
Alexander Chernykh <chernykh@me.com>
2018-01-30 11:21:54 +0300

    web: bugfixes 2.1.1 & 2.1.2

--- src/app/core/env.js
+++ src/app/core/env.js
@@ -1,4 +1,4 @@
-var build = '413';
+var build = '417';
 var version = '2.1.0';
 var protocol = {
   ws: 'wss://',
@@ -6,7 +6,7 @@ var protocol = {
 };
 var server = 'testapp.staminity.com:8080';
 var content = 'https://testapp.staminity.com:8080';
-var frontend = 'https://dev2.staminity.com/';
+var frontend = 'http://dev1.staminity.com/';
 
 
 module.exports = {
