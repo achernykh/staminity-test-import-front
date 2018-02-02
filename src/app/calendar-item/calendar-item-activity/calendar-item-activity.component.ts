f12dcd2009603a9297546ca46bab6872b98c2d88
Alexander Chernykh <chernykh@me.com>
2018-02-02 12:30:54 +0300

    mobile upd: fix prepareTemplates

--- src/app/calendar-item/calendar-item-activity/calendar-item-activity.component.ts
+++ src/app/calendar-item/calendar-item-activity/calendar-item-activity.component.ts
@@ -263,7 +263,8 @@ export class CalendarItemActivityCtrl implements IComponentController{
 
     prepareTemplates(): void {
         this.templates = this.ReferenceService.templates;
-        this.activity.header.template = this.templates.filter(t => t.id === this.activity.header.templateId)[0] || null;
+        this.activity.header.template = this.templates &&
+            this.templates.filter(t => t.id === this.activity.header.templateId)[0] || null;
         this.ReferenceService.templatesChanges
             .takeUntil(this.destroy)
             .subscribe((templates) => {
