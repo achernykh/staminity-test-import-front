b55e500585b2cb265c86205091d750196493d631
Alexander Chernykh <chernykh@me.com>
2018-02-07 10:14:21 +0300

    fix current user select in calendar

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -124,7 +124,9 @@ export class CalendarCtrl implements IComponentController{
      * @param userId
      */
     setOwner (userId: number): void {
-        this.owner = this.athletes.filter(a => a.userId === userId)[0];
+        this.owner = this.currentUser.userId === userId ?
+            this.currentUser :
+            this.athletes.filter(a => a.userId === userId)[0];
         this.$location.search('userId', this.owner.userId);
         this.setData();
     }
