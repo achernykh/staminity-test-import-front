19607c83ec39513790507fa918dccf548a7cb913
Alexander Chernykh <chernykh@me.com>
2018-03-08 19:36:50 +0300

    web: upd training plan

--- src/app/core/env.js
+++ src/app/core/env.js
@@ -1,12 +1,12 @@
-var build = '463';
+var build = '465';
 var version = '2.1.5';
 var protocol = {
-  ws: 'ws://',
-  rest: 'http://'
+  ws: 'wss://',
+  rest: 'https://'
 };
-var server = 'node51397-env-7312598.mircloud.ru:11123';
-var content = 'http://testapp.staminity.com:8080';
-var frontend = 'http://node51402-env-1004830.mircloud.ru:11159/';
+var server = 'testapp.staminity.com:8080';
+var content = 'https://testapp.staminity.com:8080';
+var frontend = 'http://dev1.staminity.com/';
 
 
 module.exports = {
