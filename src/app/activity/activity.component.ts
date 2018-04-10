9dc96815290114782e083b4b4f83b06843c787f3
Alexander Chernykh <chernykh@me.com>
2018-04-10 18:51:03 +0300

    web upd: activity state

--- src/app/activity/activity.component.ts
+++ src/app/activity/activity.component.ts
@@ -2,20 +2,30 @@ import {IComponentController, IComponentOptions, IPromise} from "angular";
 import {ICalendarItem} from "../../../api/calendar/calendar.interface";
 import {IUserProfile} from "../../../api/user/user.interface";
 import "./activity.component.scss";
+import {SessionService} from "@app/core";
+import {ICalendarItemDialogOptions} from "@app/calendar-item/calendar-item-dialog.interface";
+import {FormMode} from "../application.interface";
 
 export class ActivityCtrl implements IComponentController {
 
     item: ICalendarItem;
+    options: ICalendarItemDialogOptions;
     athlete: IUserProfile;
     onEvent: (response: Object) => IPromise<void>;
-    static $inject = [];
+    static $inject = ['SessionService'];
 
-    constructor() {
+    constructor(private session: SessionService) {
 
     }
 
     $onInit() {
-
+        this.options = {
+            currentUser: this.session.getUser(),
+            owner: this.session.getUser(),
+            popupMode: true,
+            formMode: FormMode.View,
+            trainingPlanMode: false,
+        }
     }
 }
 
@@ -23,6 +33,7 @@ const ActivityComponent: IComponentOptions = {
     bindings: {
         item: "<",
         athlete: "<",
+        options: '<',
         onEvent: "&",
     },
     require: {
