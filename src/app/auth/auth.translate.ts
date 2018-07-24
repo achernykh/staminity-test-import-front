0921c3eddae3e7eb69036305484448f13ed69675
ezakhar <e.zakhar@gmail.com>
2018-07-24 17:47:46 +0300

    minor changes

--- src/app/auth/auth.translate.ts
+++ src/app/auth/auth.translate.ts
@@ -10,7 +10,7 @@ export let _translate = {
         required: "Обязательное поле",
         err30: "{{field}} максимум 30 символов",
         errMail: "Проверьте правильность указания e-mail",
-        errPass: "Пароль должен быть не менее 8 символов и содержать хотя бы одну цифру и заглавную букву",
+        errPass: "Не менее 8 символов, включая цифру и заглавную букву",
         errComparePass: "Пароли не совпадают",
         confirmation: "Я принимаю <a href='https://legal.staminity.com/ru/license.html'>Публичную оферту</a> и соглашаюсь с <a href='https://legal.staminity.com/ru/terms.html'>Условиями использования</a> сайта.<br>",
 
@@ -121,7 +121,7 @@ export let _translate = {
         required: "Required",
         err30: "{{field}} 30 characters maximum",
         errMail: "Please check your e-mail address",
-        errPass: "The password should contain at least 8 characters and include one digit and one uppercase letter",
+        errPass: "At least 8 characters includes one digit and one capital letter",
         errComparePass: "Passwords did not match",
         confirmation: "I accept <a href='https://legal.staminity.com/en/license.html'>the Public offer</a> and agree with <a href='https://legal.staminity.com/en/terms.html'>the Website use policy</a>",
 
