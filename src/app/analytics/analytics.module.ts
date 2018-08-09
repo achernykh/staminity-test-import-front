df65ec3cb07e648a5ab964b4e398794b3d559d2a
Alexander Chernykh <chernykh@me.com>
2018-08-09 12:58:32 +0300

    web: premium release

--- src/app/analytics/analytics.module.ts
+++ src/app/analytics/analytics.module.ts
@@ -6,6 +6,7 @@ import AnalyticsManagementPanelComponent from "./analytics-management-panel/anal
 import AnalyticsComponent from "./analytics.component";
 import configure from "./analytics.config";
 import {DefaultAnalyticsSettings} from "./analytics.default";
+import {AnalyticsChartPmcComponent} from "./analytics-chart-pmc/analytics-chart-pmc.component";
 
 const Analytics = module("staminity.analytics", [])
     .component("analytics", AnalyticsComponent)
@@ -13,6 +14,7 @@ const Analytics = module("staminity.analytics", [])
     .component("analyticsChart", AnalyticsChartComponent)
     .component("analyticsChartFilter", AnalyticsChartFilterComponent)
     .component("analyticsChartSettings", AnalyticsChartSettingsComponent)
+    .component("analyticsChartPmc", AnalyticsChartPmcComponent)
     .constant("analyticsDefaultSettings", DefaultAnalyticsSettings)
     .config(configure)
     .name;
