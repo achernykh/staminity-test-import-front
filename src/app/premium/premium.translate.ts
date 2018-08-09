fd936dc5ff3570a7a2732f6303f3044a53afbc3d
ezakhar <e.zakhar@gmail.com>
2018-08-09 23:04:47 +0300

    premium features translation

--- src/app/premium/premium.translate.ts
+++ src/app/premium/premium.translate.ts
@@ -17,11 +17,12 @@ export const premiumTranslate = {
             completePercent: 'Отчет "Процент выполнения" покажет динамику среднего процента выполнения плановых тренировок за выбранный период',
 
             // others
-            activityDetails: '',
-            methodologyCategories: '',
-            methodologyTemplates: '',
-            methodologyPeriodizationSchemes: '',
-            seasonPlanning: '',
+            activityDetails: 'Панель показателей и пиков. <br>' +
+            'Отображает набор рассчитанных показателей и пиковых значений по времени/расстоянию по всей тренировке, по выбранным кругам или сегментам, а также по любому произвольно выделенному на графике отрезку тренировки',
+            methodologyCategories: 'Окно управления категориями тренировок служит для создания и изменения собственных категорий. Категории помогут классифицировать тренировки по типам, например "Длинный бег", "Интервалы ПАНО", "Темповый бег"',
+            methodologyTemplates: 'Окно управления шаблонами тренировок предназначено для создния и изменения шаблонов тренировочных заданий. Шаблоны помогают быстро планировать тренировки',
+            methodologyPeriodizationSchemes: 'В окне управления схемами периодизации можно создавать и изменять наборы мезоциклов для планирования сезона.',
+            seasonPlanning: 'В окне создания планов на сезон можно запланировать сезон по мезоциклам, указав плановые значения в каждом микроцикле по дистанции, времени или баллам нагрузки, а также контролировать выполнение плана',
 
 
         },
@@ -44,11 +45,11 @@ export const premiumTranslate = {
         },
         categories: {
             title: 'Управление категориями тренировок',
-            text: 'Создавайте свои категории тренировок по видам спорта, используйте их для классификации заданий',
+            text: 'Создавайте свои категории тренировок по видам спорта, используйте их для классификации заданий. Например "Длинный бег", "Фартлек", "Интервалы ПАНО", ...',
         },
         templates: {
             title: 'Управление шаблонами тренировок',
-            text: 'Создавайте свои шаблоны тренировок, сохраняйте в шаблоны ранее запланированные тренировки для быстрого создания нового плана',
+            text: 'Создавайте свои шаблоны тренировок, сохраняйте в шаблоны ранее запланированные тренировки для быстрого создания новых заданий',
         },
     },
     en: {
@@ -57,7 +58,25 @@ export const premiumTranslate = {
             button: 'Learn more about Premium'
         },
         functions: {
-            pmc: 'The Fitness - Fatigue chart can help you track and plan your Fitness level to ensure that you are gaining enough training and recovering sufficiently'
+            // reports
+            pmc: 'The Fitness - Fatigue chart can help you track and plan your Fitness level to ensure that you are gaining enough training and recovering sufficiently',
+            activityMeasures: 'The "Workout indicators" chart can help to evaluate the dynamics of the selected indicators for the selected date range',
+            timeInZonesHR: 'The "Time in HR Zones" can help you track the time distribution in Heart Rate training zones',
+            timeInZonesPower: 'The "Time in Power Zones" can help you track the time distribution in Power training zones',
+            timeInZonesSpeed: 'The "Time in Speed/Pace Zones" can help you track the time distribution in Heart Rate training zones',
+            HRTimePeaks: 'The "HR Time Peaks" chart can help you to know the maximum Heart Rate values for the time peaks: 2s, 5s, 10s, ... ',
+            SpeedTimePeaks: 'The "Speed/Pace Time Peaks" chart can help you to know the maximum Pace/Speed values for the time peaks: 2s, 5s, 10s, ... ',
+            PowerTimePeaks: 'The "Power Time Peaks" chart can help you to know the maximum Power values for the time peaks: 2s, 5s, 10s, ... ',
+            completePercent: 'The "Complete percent" chart can help you track your average Complete percent of planned activities indicator',
+
+            // others
+            activityDetails: 'Indicators and Peaks panel. <br>' +
+            'It contains the set of calculated indicators and Time/Distance Peaks for the whole workout, for the selected laps/segments or for any part of the workout, selected on the Indicators chart',
+            methodologyCategories: 'Workout categories window helps you to manage categories to classify the workouts. E.g. "Long run", "LT Intervals", "Fartlek" etc.',
+            methodologyTemplates: 'Workout templates window helps you to manage templates for rapid workout planning.',
+            methodologyPeriodizationSchemes: 'Periodizaton schemes window helps you to manage mesocyles set used for season planning.',
+            seasonPlanning: 'Season planning window helps you to create and manage training season plan, set planned values for every microcycle and control the plan completion.',
+
         },
         futurePlanning: {
             title: 'Workout planning',
