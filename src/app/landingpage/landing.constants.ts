e5abfe597cd4fffd5d22b1032e4d493f8803c648
ezakhar <e.zakhar@gmail.com>
2018-08-28 23:05:15 +0300

    update landing

--- src/app/landingpage/landing.constants.ts
+++ src/app/landingpage/landing.constants.ts
@@ -749,7 +749,7 @@ export const landingConfig: LandingConfig = {
     footer: [
         {
             code: 'product',
-            links: ['link1', 'link2', 'link3', 'link4', 'link5', 'link6', 'link7']
+            links: ['link1', 'link2', 'link3', 'link4']
         },
         {
             code: 'scenarios',
