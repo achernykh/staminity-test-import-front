c53bd70d79dc6942ad767646cbad7ff56a642e42
Alexander Chernykh <chernykh@me.com>
2018-09-08 15:38:09 +0300

    mobile: yandex metric

--- src/app/share/google/google-analitics.functions.ts
+++ src/app/share/google/google-analitics.functions.ts
@@ -1,6 +1,8 @@
 interface WindowGoogle extends Window {dataLayer: any;}
 
-export const gtmEvent = (params: any) => (window as WindowGoogle).dataLayer.push(params);
+export const gtmEvent = (params: any) => {
+    window['dataLayer'].push(params);
+};
 
 export const gaEmailSignup = () => {
     (window as WindowGoogle).dataLayer('send', 'event', { eventCategory: 'signup', eventAction: 'click', eventLabel: 'emailsignup'});
