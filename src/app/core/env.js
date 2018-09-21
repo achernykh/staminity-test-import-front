7daa64329c3f90c5eec5e4c581fff5fe8cbb89b1
Alexander Chernykh <chernykh@me.com>
2018-09-21 17:09:42 +0300

    premium upd

--- src/app/core/env.js
+++ src/app/core/env.js
@@ -1,10 +1,10 @@
-var build = '759';
+var build = '763';
 var version = '2.3.12';
 var protocol = {
   ws: 'wss://',
   rest: 'https://'
 };
-var server = 'testapp.staminity.com';
+var server = 'app.staminity.com';
 var content = 'https://app.staminity.com';
 var frontend = 'https://dev2.staminity.com/';
 
