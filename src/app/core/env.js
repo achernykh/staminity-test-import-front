39fa835503adbba970a10129c7bdcfd363e734ae
Alexander Chernykh <chernykh@me.com>
2018-10-24 20:30:55 +0300

    web upd: small update new-landing

--- src/app/core/env.js
+++ src/app/core/env.js
@@ -1,12 +1,12 @@
-var build = '747';
-var version = '3.0.1';
+var build = '748';
+var version = '3.1.3';
 var protocol = {
   ws: 'wss://',
   rest: 'https://'
 };
-var server = 'testapp.staminity.com';
-var content = 'https://testapp.staminity.com';
-var frontend = 'http://dev1.staminity.com/';
+var server = 'app.staminity.com';
+var content = 'https://app.staminity.com';
+var frontend = 'https://dev2.staminity.com/';
 
 
 module.exports = {
